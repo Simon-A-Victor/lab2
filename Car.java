6c13a278880016604d6a83a762add25748975763
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-11-06 13:52:40 +0100

    Update visibility

--- Car.java
+++ Car.java
@@ -1,11 +1,11 @@
 import java.awt.*;
 
 abstract class Car {
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    protected String modelName; // The car model name
 
     public int getNrDoors(){
         return nrDoors;
