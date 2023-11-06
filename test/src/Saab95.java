37ec251666f59363a40ac74b7e68cf5f4b3e40ad
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-11-06 14:22:32 +0100

    Create test folder

---
+++ test/src/Saab95.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+
+public class Saab95 extends Car{
+
+    private boolean turboOn;
+
+    
+    public Saab95(){
+        nrDoors = 2;
+        color = Color.red;
+        enginePower = 125;
+	    turboOn = false;
+        modelName = "Saab95";
+        stopEngine();
+    }
+    
+
+    public void setTurboOn(){
+	    turboOn = true;
+    }
+
+    public void setTurboOff(){
+	    turboOn = false;
+    }
+    
+    protected double speedFactor(){
+        double turbo = 1;
+        if(turboOn) turbo = 1.3;
+        return enginePower * 0.01 * turbo;
+    }
+
+    public void incrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+    }
+    
+
+    }
+
