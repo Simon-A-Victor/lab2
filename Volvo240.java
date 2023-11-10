8508ada6fce0b21526d052f9311f73281d08c11d
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-11-10 14:00:21 +0100

    Remover overriding of increaseSpeed() and decrementSpeed()

--- Volvo240.java
+++ Volvo240.java
@@ -1,22 +1,15 @@
 import java.awt.*;
 
-public class Volvo240 extends Car{
+public class Volvo240 extends Car {
 
     private final static double trimFactor = 1.25;
-    
-    public Volvo240(double x, double y){
+
+    public Volvo240(double x, double y) {
         super(4, 100, Color.black, "Volvo240", x, y);
     }
-    
-    protected double speedFactor(){
+
+    protected double speedFactor() {
         return enginePower * 0.01 * trimFactor;
     }
 
-    public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
-    }
 }
