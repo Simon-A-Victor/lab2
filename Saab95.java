592ce39a4da303ba3e548e94d2ac6da2b0e4184f
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-11-10 13:59:44 +0100

    Remover overriding of increaseSpeed() and decrementSpeed()

--- Saab95.java
+++ Saab95.java
@@ -1,38 +1,28 @@
 import java.awt.*;
 
-public class Saab95 extends Car{
+public class Saab95 extends Car {
 
     private boolean turboOn;
 
-    
-    public Saab95(double x, double y){
+
+    public Saab95(double x, double y) {
         super(2, 125, Color.red, "Saab95", x, y);
-	    turboOn = false;
-    }
-    
-
-    public void setTurboOn(){
-	    turboOn = true;
+        turboOn = false;
     }
 
-    public void setTurboOff(){
-	    turboOn = false;
+
+    public void setTurboOn() {
+        turboOn = true;
     }
-    
-    protected double speedFactor(){
+
+    public void setTurboOff() {
+        turboOn = false;
+    }
+
+    protected double speedFactor() {
         double turbo = 1;
-        if(turboOn) turbo = 1.3;
+        if (turboOn) turbo = 1.3;
         return enginePower * 0.01 * turbo;
     }
-
-    public void incrementSpeed(double amount){
-        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
-    }
-
-    public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
-    }
-    
-
-    }
+}
 
