f1e21933bb148395acbc9ab78c1ed1e93adeeeba
Oliver <o.anderzen@gmail.com>
2023-11-10 14:49:46 +0100

    Change tiltPlatform method for semiTruck

--- SemiTruck.java
+++ SemiTruck.java
@@ -1,13 +1,23 @@
 import java.awt.*;
 
-public class SemiTruck extends Car {
+public class SemiTruck extends platformVehicle {
 
     public SemiTruck(double x, double y){
         super(2,250, Color.black,"Semi69",x, y);
     }
 
+    public void tiltPlatform() {
+        if(this.isStationary()){
+            if(this.platformIsDown()){
+                this.setPlatformAngle(0);
+            } else {
+                this.setPlatformAngle(70);
+            }
+        }
+    }
+
     @Override
     double speedFactor() {
-        return 0;
+        return 1;
     }
 }
