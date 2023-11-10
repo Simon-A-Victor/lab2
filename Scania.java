8013884fab73baf52177cc6f661072bafad15861
Oliver <o.anderzen@gmail.com>
2023-11-10 13:38:34 +0100

    Implement methods and variables inside Scania

--- Scania.java
+++ Scania.java
@@ -1,3 +1,25 @@
+import java.awt.*;
+
 public class Scania extends Car{
 
+    public Scania(){
+        nrDoors = 2;
+        color = Color.black;
+        enginePower = 250;
+        modelName = "Scania";
+        stopEngine();
+    }
+    @Override
+    double speedFactor() {
+        return 0;
+    }
+
+    @Override
+    public void incrementSpeed(double amount){
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+    }
 }
