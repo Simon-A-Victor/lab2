49dca28dd23d4e37755b2d42849be3c1cd6c7723
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-11-15 09:08:01 +0100

    implement Workshop Class

--- Workshop.java
+++ Workshop.java
@@ -1,2 +1,33 @@
-public class Workshop {
+public class Workshop implements Loader{
+    private double x;
+    private double y;
+    private int maxSize;
+    private int capacity;
+
+    public void Workshop(double x, double y, int maxSize, int capacity){
+        this.x = x;
+        this.y = y;
+        this.maxSize = maxSize;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public void load(Loadable other) {
+
+    }
+
+    @Override
+    public void unload() {
+
+    }
+
+    @Override
+    public int getMaxSize() {
+        return 0;
+    }
+
+    @Override
+    public int getCapacity() {
+        return 0;
+    }
 }
