f2fbfc7c54d6c77d6b64fd735422f25fb4e8c5fa
gustav <gustav@hartwig.nu>
2023-11-15 08:45:02 +0100

    Updated method used for updating state of loaded

--- SemiTruck.java
+++ SemiTruck.java
@@ -81,36 +81,19 @@ public class SemiTruck extends PlatformVehicle implements Loader{
         Loadable unloaded = this.loaded.pop();
         return unloaded;
     }
-    @Override
-    public void gas(double amount){
-        if (0 <= amount && amount <= 1 && platformIsUp()){
-            this.incrementSpeed(amount);
-            for (Loadable loadable : loaded) {
-                loadable.setCurrentSpeed(this.getCurrentSpeed());
-            }
-        }
-    }
-    @Override
-    public void brake(double amount){
-        if (0 <= amount && amount <= 1){
-            this.decrementSpeed(amount);
-            for (Loadable loadable : loaded) {
-                loadable.setCurrentSpeed(this.getCurrentSpeed());
-            }
-        }
-    }
+
     @Override
     public void turnLeft() {
         this.setDirection(Directions.values()[(this.getDirection().ordinal()+3)%4]);
         for (Loadable loadable : loaded) {
-            loadable.turnLeft();
+            loadable.setDirection(this.getDirection());
         }
     }
     @Override
     public void turnRight() {
         this.setDirection(Directions.values()[(this.getDirection().ordinal()+1)%4]);
         for (Loadable loadable : loaded) {
-            loadable.turnRight();
+            loadable.setDirection(this.getDirection());
         }
     }
 
@@ -131,7 +114,8 @@ public class SemiTruck extends PlatformVehicle implements Loader{
                 break;
         }
         for (Loadable loadable : loaded) {
-            loadable.move();
+            loadable.setXPosition(this.getXPosition());
+            loadable.setYPosition(this.getYPosition());
         }
     }
 
