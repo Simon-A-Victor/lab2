3c29bda279342bca6a67b209c6e0de379f8df39b
gustav <gustav@hartwig.nu>
2023-11-15 09:04:37 +0100

    Make PlatformVehicle subclass of Vehicle

--- PlatformVehicle.java
+++ PlatformVehicle.java
@@ -1,13 +1,30 @@
 import java.awt.*;
 import java.util.Stack;
 
-abstract class PlatformVehicle extends Car {
+abstract class PlatformVehicle extends Vehicle {
     private double platformAngle = 0.0;
-
+    private int nrDoors;
+    private double enginePower;
+    private boolean loaded;
+    private int size;
+    private double getEnginePower;
+    abstract double speedFactor();
 
 
     public PlatformVehicle(int nrDoors, double enginePower, Color color, String modelName, double x, double y, int size) {
-        super(nrDoors, enginePower, color, modelName, x, y, size);
+        super(color, modelName, x, y);
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.loaded = false;
+        this.size = size;
+
+    }
+    protected void incrementSpeed(double amount){
+        setCurrentSpeed(Math.min(getCurrentSpeed() + speedFactor() * amount,getEnginePower()));
+    }
+
+    protected    void decrementSpeed(double amount){
+        setCurrentSpeed(Math.max(getCurrentSpeed() - speedFactor() * amount,0));
     }
 
     protected double getPlatformAngle(){
@@ -21,14 +38,24 @@ abstract class PlatformVehicle extends Car {
     protected boolean platformIsUp(){
         return getPlatformAngle() == 0.0;
     }
+    public double getEnginePower(){
+        return enginePower;
+    }
+    public boolean isStationary(){
+        double speed = getCurrentSpeed();
+        return speed == 0;
+    }
 
-    @Override
     public void gas(double amount){
         if (0 <= amount && amount <= 1 && platformIsUp()){
             this.incrementSpeed(amount);
         }
     }
+    public void brake(double amount){
+        if (0 <= amount && amount <= 1){
+            decrementSpeed(amount);
+        }
 
 
 
-}
+}}
