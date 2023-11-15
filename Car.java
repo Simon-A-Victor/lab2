c397fb6ebebb4b8500a63e874a28c958537e71e7
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-11-15 08:46:39 +0100

    add Vehicle Class

--- Car.java
+++ Car.java
@@ -3,58 +3,26 @@ import java.awt.*;
 
 
 
-abstract class Car implements Movable, Loadable {
+abstract class Car extends Vehicle implements Movable, Loadable {
 
     private int nrDoors; // Number of doors on the car
     private double enginePower; // Engine power of the car
-    private double currentSpeed; // The current speed of the car
-    private Color color; // Color of the car
-    private String modelName; // The car model name
-    private Directions direction;
-
     private boolean loaded;
-
     private int size;
 
-    private double x;
-    private double y;
-
     public Car(int nrDoors, double enginePower, Color color, String modelName, double x, double y, int size){
+        super(color, modelName, x, y);
         this.nrDoors = nrDoors;
         this.enginePower = enginePower;
-        this.color = color;
-        this.modelName = modelName;
-        this.x = x;
-        this.y = y;
-        this.size = size;
         this.stopEngine();
-        this.direction  = Directions.NORTH;
         this.loaded = false;
+        this.size = size;
     }
 
-    @Override
     public boolean isLoaded() {
         return loaded;
     }
 
-    public Directions getDirection(){
-        return direction;
-    }
-
-    public void setDirection(Directions newDirection){
-        direction = newDirection;
-    }
-
-    public double getXPosition(){
-        return x;
-    }
-
-    public double getYPosition(){
-        return y;
-    }
-
-    public void setXPosition(double amount){x = amount;}
-    public void setYPosition(double amount){y = amount;}
 
     public int getNrDoors(){
         return nrDoors;
@@ -63,88 +31,47 @@ abstract class Car implements Movable, Loadable {
         return enginePower;
     }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
 
-    @Override
-    public void setCurrentSpeed(double speed){
-        currentSpeed = speed;
-    }
+
+
 
     public boolean isStationary(){
         double speed = getCurrentSpeed();
         return speed == 0;
     }
 
-    public Color getColor(){
-        return color;
-    }
 
-    public void setColor(Color clr){
-        color = clr;
-    }
+
+
 
     public void startEngine(){
-        currentSpeed = 0.1;
+         this.setCurrentSpeed(0.1);
     }
 
     public void stopEngine(){
-        currentSpeed = 0;
+        this.setCurrentSpeed(0.1);
     }
 
     abstract double speedFactor();
 
     protected void incrementSpeed(double amount){
-        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,getEnginePower());
+        setCurrentSpeed(Math.min(getCurrentSpeed() + speedFactor() * amount,getEnginePower()));
     }
 
     protected    void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+        setCurrentSpeed(Math.max(getCurrentSpeed() - speedFactor() * amount,0));
     }
-
-
     public void gas(double amount){
         if (0 <= amount && amount <= 1){
             this.incrementSpeed(amount);
         }
     }
-
     public void brake(double amount){
         if (0 <= amount && amount <= 1){
             decrementSpeed(amount);
         }
 
     }
-
     @Override
-    public void move() {
-        switch (this.getDirection()){
-            case NORTH:
-                this.setYPosition(this.getYPosition() + this.getCurrentSpeed());
-                break;
-            case WEST:
-                this.setXPosition(this.getXPosition() - this.getCurrentSpeed());
-                break;
-            case SOUTH:
-                this.setYPosition(this.getYPosition() - this.getCurrentSpeed());
-                break;
-            case EAST:
-                this.setXPosition(this.getXPosition() + this.getCurrentSpeed());
-                break;
-        }
-    }
-
-    @Override
-    public void turnLeft() {
-        this.setDirection(Directions.values()[(direction.ordinal()+3)%4]);
-    }
-
-    @Override
-    public void turnRight() {
-        this.setDirection(Directions.values()[(direction.ordinal()+1)%4]);
-    }
-
-    @Override
-    public int getSize(){ return size;}
+    public int getSize(){ return this.size;}
 }
---
+++ Vehicle.java
@@ -0,0 +1,88 @@
+import java.awt.*;
+
+public class Vehicle implements Movable {
+    private double currentSpeed;
+    private Color color;
+    private String modelName;
+    private Movable.Directions direction;
+    private double x;
+    private double y;
+
+    public Vehicle(Color color, String modelName, double x, double y){
+        this.color = color;
+        this.modelName = modelName;
+        this.x = x;
+        this.y = y;
+    }
+    public Color getColor(){
+        return color;
+    }
+    public void setColor(Color clr){
+        color = clr;
+    }
+    @Override
+    public void move() {
+        switch (this.getDirection()){
+            case NORTH:
+                this.setYPosition(this.getYPosition() + this.getCurrentSpeed());
+                break;
+            case WEST:
+                this.setXPosition(this.getXPosition() - this.getCurrentSpeed());
+                break;
+            case SOUTH:
+                this.setYPosition(this.getYPosition() - this.getCurrentSpeed());
+                break;
+            case EAST:
+                this.setXPosition(this.getXPosition() + this.getCurrentSpeed());
+                break;
+        }
+    }
+    @Override
+    public void turnLeft() {
+        this.setDirection(Directions.values()[(direction.ordinal()+3)%4]);
+    }
+
+    @Override
+    public void turnRight() {
+        this.setDirection(Directions.values()[(direction.ordinal()+1)%4]);
+    }
+
+    @Override
+    public double getXPosition() {
+        return this.x;
+    }
+
+    @Override
+    public double getYPosition() {
+        return this.y;
+    }
+
+    @Override
+    public void setXPosition(double amount) {
+        this.x = amount;
+    }
+
+    @Override
+    public void setYPosition(double amount) {
+        this.y = amount;
+    }
+
+    @Override
+    public void setDirection(Directions direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public Directions getDirection() {
+        return this.direction;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    @Override
+    public void setCurrentSpeed(double speed){
+        currentSpeed = speed;
+    }
+}
